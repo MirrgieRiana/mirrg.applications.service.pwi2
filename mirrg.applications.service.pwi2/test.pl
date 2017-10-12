d7844c42a9d1b8471825d10aa9b1a5e3f65d034d
Mirrgie Riana <tacticsrealize2@gmail.com>
2017-10-12 21:33:37 +0900

    add: test.pl

---
+++ mirrg.applications.service.pwi2/test.pl
@@ -0,0 +1,36 @@
+
+local $SIG{HUP} = sub {
+	print "HUP!";
+	sleep(5);
+	exit(0);
+};
+
+local $SIG{TERM} = sub {
+	print "TERM!";
+	sleep(5);
+	exit(0);
+};
+
+local $SIG{INT} = sub {
+	print "INT!";
+	sleep(5);
+	exit(0);
+};
+
+$| = 1;
+
+print "process started", "\n";
+
+while (<>) {
+	chomp $_;
+	if ($_ eq "stop") {
+		print "stopped", "\n";
+		exit(0);
+	}
+	print $_, "\n";
+	sleep(1);
+	print $_, "\n";
+}
+
+print "stdin closed", "\n";
+print "stopped", "\n";
